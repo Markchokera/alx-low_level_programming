fa0161e11035733eb3941ef8545b3299b9c6d43e
root <markchokera>
2023-03-18 02:02:14 -0700

    Hello world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("Size of char: %ld bytes\n", sizeof(char));
+    printf("Size of int: %ld bytes\n", sizeof(int));
+    printf("Size of short: %ld bytes\n", sizeof(short));
+    printf("Size of long: %ld bytes\n", sizeof(long));
+    printf("Size of float: %ld bytes\n", sizeof(float));
+    printf("Size of double: %ld bytes\n", sizeof(double));
+    printf("Size of long double: %ld bytes\n", sizeof(long double));
+    printf("Size of pointer: %ld bytes\n", sizeof(void*));
+    return 0;
+}
