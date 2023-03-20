ab227ab31584da68e7587aebbbb6a59ec5254109
root <markchokera>
2023-03-20 13:19:35 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-        char i;
+	char i;
 
-        for (i = 'a' ; i <= 'z' ; i++)
-                putchar(i);
-        putchar('\n');
-        return (0);
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar ('\n');
+	return (0);
 }
