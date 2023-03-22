5c619741470c9b0b555a46b8502ee4605bf0d479
root <markchokera>
2023-03-21 18:40:05 -0700

    Sign

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int r;
-	
+
 	r = print_sign(98);
 	_putchar(',');
 	_putchar(' ');
@@ -29,5 +29,5 @@ int main(void)
 	_putchar(' ');
 	_putchar(r + '0');
 	_putchar('\n');
-       	return (0);
+	return (0);
 }
