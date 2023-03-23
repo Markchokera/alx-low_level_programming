931232ba68feecea65636cd1dac9912a8ec53eaa
root <markchokera>
2023-03-23 12:22:59 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,18 +16,18 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (a > b && c > a)
-	{
-		largest = c;
-	}
-	else if (b > c)
+	else if (b > c && b > a)
 	{
 		largest = b;
 	}
+	else if (c > b)
+	{
+		largest = c;
+	}
 	else
 	{
-		largest = c
+		largest = b;
 	}
-
-return (largest);
+	
+	return (largest);
 }
