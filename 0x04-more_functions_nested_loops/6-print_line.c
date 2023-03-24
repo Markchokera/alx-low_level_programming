2dd21b0590bcd42fe7acaaa795281a18ad6d4fd8
root <markchokera>
2023-03-24 05:22:35 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line -straight -line
+ * @n:No of times
+ * Return:Always 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		putchar('_');
+	}
+	putchar('\n');
+}
