16232e8f38fd0e411749d855ee602a13ee558976
root <markchokera>
2023-03-27 07:23:44 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,24 +7,11 @@
  */
 void puts2(char *str)
 {
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
+	int i;
 
-	while (*y != '\0')
+	for (i = 0; str[i] != '\0'; i += 2)
 	{
-		y++;
-
-		longi++;
-	}
-	t = loni - 1;
-	for (o = 0 ; o <= t ; o++)
-	{
-		if (o % 2 == 0)
-	{
-		_putchar(str[o]);
-	}
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
