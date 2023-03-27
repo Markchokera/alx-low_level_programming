52746a21a7e8a171042af1edde640ed5a2375d0a
root <markchokera>
2023-03-27 09:55:05 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,12 +6,13 @@
  * @src: copy from
  * Return: string
  */
+
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 	int x = 0;
 
-	while (*(src + 1) != '\0')
+	while (*(src + i) != '\0')
 	{
 		i++;
 	}
