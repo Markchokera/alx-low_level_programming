86962e56adc3024e2017b143f65493e833fe2399
root <markchokera>
2023-03-30 04:24:53 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN.H
 #define MAIN_H
 
-/**
- * This header file has all prototypes for the 0x06. C - More pointers, arrays and strings
- */
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -17,4 +14,5 @@ char *leet(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+
 #endif
