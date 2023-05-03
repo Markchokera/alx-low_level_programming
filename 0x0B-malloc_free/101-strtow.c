a3a127b73c8fdee69a1c0f69140844bfb8a0046b
Markchokera <markchokera@gmail.com>
2023-05-03 11:51:20 +0300

    Updated 101-strtow.c and README.md and added 0-print_listint.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -28,13 +28,13 @@ int count_word(char *s)
 	return (w);
 }
 /**
- * **strtow - splits a string into words
+ * **strtoq - splits a string into words
  * @str: string to split
  *
  * Return: pointer to an array of strings (Success)
  * or NULL (Error)
  */
-char **strtow(char *str)
+char **strtoq(char *str)
 {
 	char **matrix, *tmp;
 	int i, k = 0, len = 0, words, c = 0, start, end;
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Alx-low_level_programming
+0x13. C - More singly linked lists
