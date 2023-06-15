aeb8170e11dc600aa7be4551b24107bc50b82315
Markchokera <markchokera@gmail.com>
2023-06-15 22:32:12 +0300

    updated

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -8,17 +8,18 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 {
+	unsigned int count = 0;
 	dlistint_t *current = head;
-	unsigned int i = 0;
 
 	while (current != NULL)
 	{
-		if (i == index)
+		if (count == index)
 			return (current);
 
+		count++;
 		current = current->next;
-		i++;
 	}
 
 	return (NULL);
 }
+
